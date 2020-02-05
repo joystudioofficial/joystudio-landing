9edef1497671321ea5f637db43b31bc43578f27d
farhannhdyt <inzanePlayers1945@gmail.com>
2020-02-05 22:45:50 +0700

    Landing page js 20%

---
+++ src/components/LandingArea.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="js-landing-area">
+      <div class="container">
+        <h1>This is landing area</h1>
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,8 +6,8 @@
           <a href="http://" class="js-navigation-logo">Joy <span style="color: #1DD1A1;">Studio.</span></a>
         </div>
 
-        <button class="btn btn-open-navigation-menu">
-          <span>Menu</span>
+        <button class="btn btn-open-side-bar" id="btnOpenSideMenu">
+          <img src="@/assets/images/menu.svg" class="menu-bar" alt="" srcset="">
         </button>
 
         <div class="js-navigation-menu">
@@ -20,10 +20,36 @@
         </div>
       </div>
     </nav>
+
+    <section class="js-side-bar-overlay" id="sidebar">
+      <aside class="js-side-bar">
+        <div class="container">
+          <div class="js-side-header">
+            <div class="js-side-menu-brand">
+              <a href="http://" class="js-side-menu-logo">Joy <span>Studio.</span></a>
+            </div>
+            <button class="btn btn-close-side-bar" id="btnCloseSideMenu">
+              <span>&times;</span>
+            </button>
+          </div>
+
+          <div class="js-side-section-menu">
+            <ul class="js-side-menu">
+              <li class="js-side-item"><a href="http://" class="js-side-link">About</a></li>
+              <li class="js-side-item"><a href="http://" class="js-side-link">Services</a></li>
+              <li class="js-side-item"><a href="http://" class="js-side-link">Portfolio</a></li>
+              <li class="js-side-item"><a href="http://" class="js-side-link">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+      </aside>
+    </section>
   </div>
 </template>
 
 <script>
+import '@/assets/front-assets/js/myscript.js'
+
 export default {
   
 }
