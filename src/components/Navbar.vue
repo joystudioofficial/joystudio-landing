e9beee7c1d2348d4c7694d3b2236d8020610d577
farhannhdyt <inzanePlayers1945@gmail.com>
2020-02-05 16:36:31 +0700

    Design landing page joy studio 15%

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <nav class="js-navigation">
+      <div class="container cf">
+        <div class="js-navigation-brand">
+          <a href="http://" class="js-navigation-logo">Joy <span style="color: #1DD1A1;">Studio.</span></a>
+        </div>
+
+        <button class="btn btn-open-navigation-menu">
+          <span>Menu</span>
+        </button>
+
+        <div class="js-navigation-menu">
+          <ul class="js-nav-menu">
+            <li class="js-nav-item"><a href="http://" class="js-nav-link">About</a></li>
+            <li class="js-nav-item"><a href="http://" class="js-nav-link">Services</a></li>
+            <li class="js-nav-item"><a href="http://" class="js-nav-link">Portfolio</a></li>
+            <li class="js-nav-item"><a href="http://" class="js-nav-link">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css?family=Poppins&display=swap');
+  @import './../assets/front-assets/css/style.css';
+</style>
